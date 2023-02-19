e37cae34e68f37dd78849efcdef1ff32411cadd2
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-02-20 00:38:23 +0900

    Time: 402 ms (50.21%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+# WITH salary_table AS (
+#      SELECT id, salary, DENSE_RANK() OVER(ORDER BY salary) sal_rank
+#      FROM employee
+#     )
+
+# SELECT IF(MAX(sal_rank)= 2, salary, null) AS SecondHighestSalary
+# FROM salary_table
+# WHERE sal_rank = 2
+
+# 2
+SELECT IF(max(R.sal_rank)=2,R.salary,null) AS 'SecondHighestSalary' 
+FROM (
+       SELECT salary
+            , DENSE_RANK() OVER(ORDER BY salary DESC) AS sal_rank
+       FROM Employee
+       ) R
+WHERE R.sal_rank = 2
