f74f9568dd9c8b27e3b65d1b3a5f0093f9897dc5
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-02-20 00:39:22 +0900

    Update 0176-second-highest-salary.sql

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
-# WITH salary_table AS (
-#      SELECT id, salary, DENSE_RANK() OVER(ORDER BY salary) sal_rank
-#      FROM employee
-#     )
+# 1
+WITH salary_table AS (
+     SELECT id, salary, DENSE_RANK() OVER(ORDER BY salary) sal_rank
+     FROM employee
+    )
 
-# SELECT IF(MAX(sal_rank)= 2, salary, null) AS SecondHighestSalary
-# FROM salary_table
-# WHERE sal_rank = 2
+SELECT IF(MAX(sal_rank)= 2, salary, null) AS SecondHighestSalary
+FROM salary_table
+WHERE sal_rank = 2
 
 # 2
 SELECT IF(max(R.sal_rank)=2,R.salary,null) AS 'SecondHighestSalary' 
