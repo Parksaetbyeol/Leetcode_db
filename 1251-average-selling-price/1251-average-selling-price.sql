6b780e465e746182d24edda2b860fc20e01f5cc7
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-04-03 14:55:26 +0900

    Time: 1708 ms (14.32%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,13 +1,13 @@
-# SELECT P.product_id,
-#     ROUND(SUM(units*price)/SUM(units), 2) AS average_price
-# FROM Prices P JOIN UnitsSold U USING(product_id)
-# WHERE purchase_date between start_date and end_date
-# GROUP BY P.product_id
-
-
 SELECT P.product_id,
     ROUND(SUM(units*price)/SUM(units), 2) AS average_price
-FROM Prices P JOIN UnitsSold U ON P.product_id = U.product_id
-    and purchase_date between start_date and end_date
+FROM Prices P JOIN UnitsSold U USING(product_id)
+WHERE purchase_date between start_date and end_date
 GROUP BY P.product_id
+
+
+# SELECT P.product_id,
+#     ROUND(SUM(units*price)/SUM(units), 2) AS average_price
+# FROM Prices P JOIN UnitsSold U ON P.product_id = U.product_id
+#     and purchase_date between start_date and end_date
+# GROUP BY P.product_id
 
