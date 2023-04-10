e1da87a2670717e14d08c259a0a37ed012ea9c16
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-04-10 23:07:28 +0900

    Time: 895 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,13 @@
+SELECT e1.name
+FROM Employee e1 JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e1.id
+HAVING COUNT(*) >= 5;
+
+# 2
+# SELECT name 
+# FROM Employee 
+# WHERE id IN 
+#     (SELECT managerId
+#      FROM Employee
+#      GROUP BY managerId
+#      HAVING COUNT(*) >= 5);
