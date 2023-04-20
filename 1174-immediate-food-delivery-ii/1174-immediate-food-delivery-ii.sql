456fcac428fad413613f28fcb5046d332351ae95
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-04-20 13:43:19 +0900

    Time: 1112 ms (74.88%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,9 +2,21 @@
 # 고객의 첫 번째 주문은 고객이 주문한 날짜가 가장 빠른 주문입니다. 고객이 정확히 하나의 첫 번째 주문을 가지고 있음을 보장합니다.
 # 모든 고객의 첫 번째 주문에서 즉시 주문 비율을 소수점 둘째 자리로 반올림하여 찾습니다.
 
-SELECT ROUND(AVG(order_date = customer_pref_delivery_date)*100, 2) AS immediate_percentage
-FROM delivery
-WHERE (customer_id, order_date) IN
-    (select customer_id, min(order_date) as first_order
-    from delivery
-    group by customer_id)
+# 1
+# SELECT ROUND(AVG(order_date = customer_pref_delivery_date)*100, 2) AS immediate_percentage
+# FROM delivery
+# WHERE (customer_id, order_date) IN
+#     (select customer_id, min(order_date) as first_order
+#     from delivery
+#     group by customer_id)
+
+
+# 2 
+SELECT ROUND(
+        (SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END))*100 
+           / COUNT(delivery_id)
+        ,2) AS immediate_percentage
+
+FROM Delivery
+WHERE (customer_id,order_date) IN 
+    (SELECT customer_id, MIN(order_date) FROM Delivery GROUP BY 1)
