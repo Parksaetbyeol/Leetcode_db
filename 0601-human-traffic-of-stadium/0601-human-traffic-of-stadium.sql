defda1c999dfae689e3a74cd6f331b6571dec7b8
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-05-01 22:27:56 +0900

    Time: 635 ms (72.04%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,22 +1,11 @@
-
-# 2 - window function
-WITH T1 AS(
-        SELECT id, id - row_number() over() as id_diff
-        FROM Stadium
-        WHERE people >= 100
-        ),
-    T2 AS(
-        SELECT *, row_number() over(partition by id_diff) as id_diff_order
-        FROM T1
-        ),
-    T3 AS(
-        SELECT id
-        FROM T2
-        WHERE id_diff IN (SELECT id_diff FROM T2 
-                            WHERE id_diff_order > 2
-                            GROUP BY id_diff)
-        )
-SELECT *
-FROM Stadium
-WHERE id IN (SELECT id FROM T3)
-ORDER BY visit_date
+WITH T1 AS (
+    SELECT *, id - row_number() over() as id_diff
+    FROM Stadium
+    WHERE people >= 100
+    )
+SELECT id, visit_date, people
+FROM T1
+WHERE id_diff IN (SELECT id_diff
+                    FROM T1 GROUP BY id_diff
+                    HAVING COUNT(*) > 2
+ORDER BY visit_date)
