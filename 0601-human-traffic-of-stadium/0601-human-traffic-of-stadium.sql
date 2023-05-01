c003dfb502fa64ad7816fbb0a7055921454d5d26
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-05-01 22:25:17 +0900

    Time: 683 ms (52.57%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,18 +1,3 @@
-# 연속된 ID가 있는 3개 이상의 행이 있는 레코드를 표시하는 SQL 쿼리를 작성하고 각 사람의 수는 100보다 크거나 같습니다.
-
-# 1 - preceding, following, very long
-# WITH T1 AS (
-#     SELECT *,
-#         COUNT(*) OVER(ORDER BY id RANGE BETWEEN current row AND 2 following) following_cnt,
-#         COUNT(*) OVER(ORDER BY id RANGE BETWEEN 2 preceding AND current row) preceding_cnt,
-#         COUNT(*) OVER(ORDER BY id RANGE BETWEEN 1 preceding AND 1 following) current_cnt
-#     FROM Stadium
-#     WHERE people >= 100)
-
-# SELECT id, visit_date, people
-# FROM T1
-# WHERE following_cnt =  3 OR preceding_cnt = 3 OR current_cnt = 3
-# ORDER BY visit_date
 
 # 2 - window function
 WITH T1 AS(
