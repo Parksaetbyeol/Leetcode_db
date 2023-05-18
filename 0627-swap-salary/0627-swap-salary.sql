cb38129c275ddb5757441d8b9d6ecf05520bd9b6
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-05-18 23:16:11 +0900

    Time: 633 ms (8.33%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,5 +1,5 @@
-UPDATE salary
-SET sex = CASE WHEN sex = 'f' 
+UPDATE salary # 테이블의 데이터를 변경
+SET sex = CASE WHEN sex = 'f'  # SET '변경할 열' = '변경할 값'
             THEN 'm' 
         ELSE 'f' 
         END
