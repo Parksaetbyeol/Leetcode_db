28f70384317b92cf52d8be708afc9f00ea2feb3e
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-05-19 23:53:09 +0900

    Time: 1056 ms (38.80%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT num AS ConsecutiveNums
 FROM (
     SELECT num
-        , LEAD(num, 1) OVER (ORDER BY id) AS 'L'
+        , LEAD(num, 1) OVER (ORDER BY id) AS 'L' # num을 기준으로 1번째 다음 행 출력
         , LEAD(num, 2) OVER (ORDER BY id) AS 'L2'
     FROM Logs) Logs2
 WHERE Logs2.num = Logs2.L AND Logs2.L = Logs2.L2;
