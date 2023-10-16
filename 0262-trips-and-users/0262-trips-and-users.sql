55edbbfca199e89d512c8bf2b55fff1aa5ec6c88
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-10-16 21:18:59 +0900

    Time: 1201 ms (54.00%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,6 +1,5 @@
 SELECT request_at AS Day
-    , ROUND((SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) 
-         / COUNT(DISTINCT id)),2) AS 'Cancellation Rate'
+    , ROUND((SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) / COUNT(DISTINCT id)),2) AS 'Cancellation Rate'
 FROM Trips
 WHERE request_at BETWEEN "2013-10-01" AND "2013-10-03"
     AND client_id NOT IN (SELECT users_id
