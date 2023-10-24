8dcf23fa30d7f852b47d641b5dedf58708870b58
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-10-24 22:20:12 +0900

    Time: 2328 ms (23.41%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -2,6 +2,7 @@ SELECT name, bonus
 FROM Employee E LEFT JOIN Bonus B USING(empID)
 WHERE bonus < 1000 or bonus IS NULL;
 
+# 2
 # SELECT name, bonus
 # FROM Employee LEFT JOIN Bonus USING(empId)
 # WHERE COALESCE(bonus, 0) < 1000; 
