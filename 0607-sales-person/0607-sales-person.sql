1279a72fafc1f14e3f12a388437cbb988a5c6a9a
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-10-27 21:44:23 +0900

    Time: 3420 ms (26.28%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,5 +1,5 @@
 SELECT name
 FROM SalesPerson
 WHERE sales_id NOT IN (SELECT sales_id 
-                        FROM Company C RIGHT JOIN Orders O ON C.com_id = O.com_id
+                        FROM Company C RIGHT JOIN Orders O ON C.com_id = O.com_id  ## USING(com_id)
                         WHERE C.name = 'RED')
